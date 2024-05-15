2ad6052826291020e62b9a86ebba47f014f013c4
lucianohanzo <68870643+lucianohanzo@users.noreply.github.com>
2024-05-15 11:06:50 -0300

    Arquivo de atualização do debian.
    Script que atualiza sistemas com base debian.

---
+++ atualiza_debian.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+clear
+echo -e "O sistema será atualizado.\n"
+
+echo "O que fazer após a atualização?"
+echo "1 - Reiniciar."
+echo "2 - Desligar."
+echo -e "* - Continuar a usar o sistema.\n"
+
+read -p "Digite a sua escolha : " Escolha
+
+
+# atualiza lista de pacotes.
+apt update && apt list --upgradable &&
+clear && echo "Lista de pacotes atualizada!" && sleep 4
+
+# Atualiza os pacotes do sistema.
+clear && apt upgrade -y
+clear && echo "Pacotes do sistema atualizados!" && sleep 2
+
+# Escolha Final
+case $Escolha in
+    1)  clear 
+        echo "Reiniciando o sistema" && sleep 4
+        reboot ;;
+    2)  clear 
+        echo "Desligando o sistema" && sleep 4
+        shutdown -h now ;;
+    *)  clear
+        echo "Sistema liberado!" && sleep 1;;
+esac
+
+
+
