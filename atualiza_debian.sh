446edf8375a6790c7974a8053d2ec51e6a998c1d
lucianohanzo <68870643+lucianohanzo@users.noreply.github.com>
2024-05-20 21:51:42 -0300

    atualiza_debian.sh

--- atualiza_debian.sh
+++ atualiza_debian.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-clear
-echo -e "O sistema será atualizado.\n"
+# Testa se o usuário é "root".
+Usu=$(whoami)
+if [ "$Usu" == "root" ]; then
+    clear && echo "Bem vindo, usuário $Usu!" && sleep 4
+else
+    clear && echo "#=== Usuário \"$Usu\", Não é permitido. ===#"
+             echo "#=== Somente usuário root! === #" && sleep 5
+    clear ; exit 1
+fi
 
-echo "O que fazer após a atualização?"
-echo "1 - Reiniciar."
-echo "2 - Desligar."
-echo -e "* - Continuar a usar o sistema.\n"
+clear
+echo -e "#=== O sistema será atualizado. ===#      |"
+echo "                                          |"
+echo "O que fazer após a atualização?           |"
+echo "1 - Reiniciar.                            |"
+echo "2 - Desligar.                             |"
+echo -e "* - Continuar a usar o sistema.           |"
+echo "                                          |"
+echo "------------------------------------------|"
 
 read -p "Digite a sua escolha : " Escolha
 
 
 # atualiza lista de pacotes.
-apt update && apt list --upgradable &&
+apt update && apt list --upgradable
 clear && echo "Lista de pacotes atualizada!" && sleep 4
 
 # Atualiza os pacotes do sistema.
@@ -30,6 +42,4 @@ case $Escolha in
     *)  clear
         echo "Sistema liberado!" && sleep 1;;
 esac
-
-
 
